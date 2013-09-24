6939fdc0499b2ff4e4e5833b050b621492001966
x1957@milab <fangyuhao@gmail.com>
2013-09-24 16:43:11 +0800

    ch5 finished half

--- ch5/operator-parser.hs
+++ ch5/operator-parser.hs
@@ -117,8 +117,14 @@ eval val@(String _) = return val
 eval val@(Number _) = return val
 eval val@(Bool _) = return val
 eval (List [Atom "quote" , val]) = return  val
+eval (List [Atom "if" , predc , conseq , alt]) = 
+  do result <- eval predc
+     case result of
+      Bool False -> eval alt
+      otherwise -> eval conseq
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
+
 
 apply ::  String -> [LispVal] -> ThrowsError LispVal
 apply func args = maybe (throwError $ NotFunction "Unrecognized primitive function args" func) ($ args) (lookup func primitives)
@@ -130,7 +136,47 @@ primitives = [("+" , numericBinop (+)),
               ("/" , numericBinop div),
               ("mod" , numericBinop mod),
               ("quotient" , numericBinop quot),
-              ("remainder" , numericBinop rem)]
+              ("remainder" , numericBinop rem),
+              ("=" , numBoolBinop (==)),
+              ("<" , numBoolBinop (<)),
+              (">" , numBoolBinop (>)),
+              ("/=" , numBoolBinop (/=)),
+              (">=" , numBoolBinop (>=)),
+              ("<=" , numBoolBinop (<=)),
+              ("&&" , boolBoolBinop (&&)),
+              ("||" , boolBoolBinop (||)),
+              ("string=?" , strBoolBinop (==)),
+              ("string?" , strBoolBinop (>)),
+              ("string<=?" , strBoolBinop (<=)),
+              ("string>=?" , strBoolBinop (>=)),
+              ("car" , car),
+              ("cdr" , cdr),
+              ("cons" , cons),
+              ("eq?" , eqv),
+              ("eqv?" , eqv)]
+
+boolBinop :: (LispVal -> ThrowsError a) -> (a -> a -> Bool) -> [LispVal] -> ThrowsError LispVal
+boolBinop unpacker op args = if length args /= 2
+                             then throwError $ NumArgs 2 args
+                             else do left <- unpacker $ args !! 0
+                                     right <- unpacker $ args !! 1
+                                     return $ Bool $ left `op` right
+
+numBoolBinop = boolBinop unpackNum
+strBoolBinop = boolBinop unpackStr
+    where 
+      unpackStr :: LispVal -> ThrowsError String
+      unpackStr (String s) = return s
+      unpackStr (Number s) = return $ show s
+      unpackStr (Bool s) = return $ show s
+      unpackStr notString = throwError $ TypeMismatch "string" notString
+
+boolBoolBinop = boolBinop unpackBool
+    where
+      unpackBool :: LispVal -> ThrowsError Bool
+      unpackBool (Bool b) = return b
+      unpackBool notBool = throwError $ TypeMismatch "boolean" notBool
+
 numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
 numericBinop op singleVal@[_] = throwError $ NumArgs 2 singleVal
 numericBinop op params = mapM unpackNum params >>= return . Number . foldl1 op
@@ -144,6 +190,39 @@ unpackNum (String n) = let parsed = reads n in
 unpackNum (List [n]) = unpackNum n
 unpackNum notNum = throwError $ TypeMismatch "number" notNum
 
+car :: [LispVal] -> ThrowsError LispVal
+car [List (x : xs)] = return x
+car [DottedList (x : xs) _] = return x
+car [badArg] = throwError $ TypeMismatch "pair" badArg
+car badArgList = throwError $ NumArgs 1 badArgList
+
+cdr :: [LispVal] -> ThrowsError LispVal
+cdr [List (x : xs)] = return $ List xs
+cdr [DottedList (_ : xs) x] = return $ DottedList xs x
+cdr [DottedList [xs] x] = return x
+cdr [badArg] = throwError $ TypeMismatch "pair" badArg
+cdr badArgList = throwError $ NumArgs 1 badArgList
+
+cons :: [LispVal] -> ThrowsError LispVal
+cons [x1 , List []] = return $ List [x1]
+cons [x , List xs] = return $ List $ [x] ++ xs
+cons [x , DottedList xs xlast] = return $ DottedList ([x] ++ xs) xlast
+cons [x1 , x2] = return $ DottedList [x1] x2
+cons badArgList = throwError $ NumArgs 2 badArgList
+
+eqv :: [LispVal] -> ThrowsError LispVal
+eqv [(Bool arg1) , (Bool arg2)] = return $ Bool $ arg1 == arg2
+eqv [(Number arg1) , (Number arg2)] = return $ Bool $ arg1 == arg2
+eqv [(String arg1) , (String arg2)] = return $ Bool $ arg1 == arg2
+eqv [(Atom arg1) , (Atom arg2)] = return $ Bool $ arg1 == arg2
+eqv [(DottedList xs x) , (DottedList ys y)] = eqv [List $ xs ++ [x] , List $ ys ++ [y]]
+eqv [(List arg1) , (List arg2)] = return $ Bool $ (length arg1 == length arg2) && (and $ map eqvPair $ zip arg1 arg2)
+    where
+      eqvPair (x1 , x2) = case eqv [x1 , x2] of
+        Left err -> False
+        Right (Bool val)-> val
+eqv [_ , _] = return $ Bool False
+eqv badArgList = throwError $ NumArgs 2 badArgList
 
 main :: IO ()
 main = do
