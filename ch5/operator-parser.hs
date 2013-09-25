a8276722e678ce1d3034aa27644bfc5a9498f0c5
x1957@milab <fangyuhao@gmail.com>
2013-09-25 20:45:30 +0800

    ch5 finished

--- ch5/operator-parser.hs
+++ ch5/operator-parser.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
 module Main where
 
 import System.Environment
@@ -153,7 +155,8 @@ primitives = [("+" , numericBinop (+)),
               ("cdr" , cdr),
               ("cons" , cons),
               ("eq?" , eqv),
-              ("eqv?" , eqv)]
+              ("eqv?" , eqv),
+              ("equal?" , equal)]
 
 boolBinop :: (LispVal -> ThrowsError a) -> (a -> a -> Bool) -> [LispVal] -> ThrowsError LispVal
 boolBinop unpacker op args = if length args /= 2
@@ -164,18 +167,18 @@ boolBinop unpacker op args = if length args /= 2
 
 numBoolBinop = boolBinop unpackNum
 strBoolBinop = boolBinop unpackStr
-    where 
-      unpackStr :: LispVal -> ThrowsError String
-      unpackStr (String s) = return s
-      unpackStr (Number s) = return $ show s
-      unpackStr (Bool s) = return $ show s
-      unpackStr notString = throwError $ TypeMismatch "string" notString
+
+unpackStr :: LispVal -> ThrowsError String
+unpackStr (String s) = return s
+unpackStr (Number s) = return $ show s
+unpackStr (Bool s) = return $ show s
+unpackStr notString = throwError $ TypeMismatch "string" notString
 
 boolBoolBinop = boolBinop unpackBool
-    where
-      unpackBool :: LispVal -> ThrowsError Bool
-      unpackBool (Bool b) = return b
-      unpackBool notBool = throwError $ TypeMismatch "boolean" notBool
+
+unpackBool :: LispVal -> ThrowsError Bool
+unpackBool (Bool b) = return b
+unpackBool notBool = throwError $ TypeMismatch "boolean" notBool
 
 numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
 numericBinop op singleVal@[_] = throwError $ NumArgs 2 singleVal
@@ -223,6 +226,23 @@ eqv [(List arg1) , (List arg2)] = return $ Bool $ (length arg1 == length arg2) &
         Right (Bool val)-> val
 eqv [_ , _] = return $ Bool False
 eqv badArgList = throwError $ NumArgs 2 badArgList
+
+data Unpacker = forall a . Eq a => AnyUnpacker (LispVal -> ThrowsError a)
+
+unpackEquals :: LispVal -> LispVal -> Unpacker -> ThrowsError Bool
+unpackEquals arg1 arg2 (AnyUnpacker unpacker) = 
+    do
+      unpacked1 <- unpacker arg1
+      unpacked2 <- unpacker arg2
+      return $ unpacked1 == unpacked2
+  `catchError` const (return False) --don't know why
+
+equal :: [LispVal] -> ThrowsError LispVal
+equal [arg1 , arg2] = do
+  primitiveEquals <- liftM or $ mapM (unpackEquals arg1 arg2) [AnyUnpacker unpackNum , AnyUnpacker unpackStr , AnyUnpacker unpackBool]
+  eqvEquals <- eqv [arg1 , arg2]
+  return $ Bool $ (primitiveEquals || let (Bool x) = eqvEquals in x)
+equal badArgList = throwError $ NumArgs 2 badArgList
 
 main :: IO ()
 main = do
